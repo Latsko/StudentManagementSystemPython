b75b4740830de58b3d8e92cd3ae1cb944840a470
Stanislav Latsko <01latsko10@gmail.com>
2025-11-06 12:31:42 +0100

    commit #4: implemented add student logic

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
-    QTableWidget, QTableWidgetItem
 import sqlite3
 import sys
+
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,6 +15,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -20,7 +23,6 @@ class MainWindow(QMainWindow):
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
         self.table = QTableWidget()
-        self.resize(400,300)
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
@@ -35,11 +37,65 @@ class MainWindow(QMainWindow):
             for column_number, column_data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
 
+        # Resize table cells to fit content
+        self.table.resizeColumnsToContents()
+        self.table.resizeRowsToContents()
+        # Adjust the window size to match the table
+        self.adjustSize()
+        self.resize(self.table.sizeHint().width() + 30,
+                    self.table.sizeHint().height() + 50)
+
         connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        self.course_name = QComboBox()
+        courses =  ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        button = QPushButton("Submit")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        student_management_sys.load_data()
+
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.load_data()
-age_calculator.show()
+student_management_sys = MainWindow()
+student_management_sys.load_data()
+student_management_sys.show()
 
 sys.exit(app.exec())
